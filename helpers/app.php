722dd2d4408880cf6f4df4df3edc8eef65a10a1f
Svobodennn <melih.sarac@hotmail.com>
2023-10-23 14:37:41 +0300

    Route, View, Asset and Language functions

--- helpers/app.php
+++ helpers/app.php
@@ -1,1 +1,41 @@
 <?php
+function route($index)
+{
+    global $config;
+    if (isset($config['route'][$index])) {
+        return $config['route'][$index];
+    } else
+        return false;
+
+}
+
+function view($viewName, $pageData = [])
+{
+    $data = $pageData;
+    if (file_exists(BASEDIR.'/View/'.$viewName.'.php')){
+        require BASEDIR.'/View/'.$viewName.'.php';
+    } else
+        return false;
+
+}
+
+function assets($assetName)
+{
+    if (file_exists(BASEDIR.'/public/'.$assetName)){
+        return URL.'public/'.$assetName;
+    } else
+        return false;
+
+}
+
+function lang($text){
+
+    global $lang;
+    if (isset($lang[$text])){
+        return $lang[$text];
+    } else
+        return $text;
+
+
+
+}
