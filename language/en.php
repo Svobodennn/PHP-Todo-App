a659037ca16a4d56ca2d198a77bf8e48903a7852
Svobodennn <melih.sarac@hotmail.com>
2023-10-23 14:36:42 +0300

    Localisation

--- language/en.php
+++ language/en.php
@@ -1,6 +1,7 @@
 <?php
 $lang = [
     'title' => 'Title',
+    'Sign in to start your session' => 'Sign in to start your session'
 
 
 ];
--- language/tr.php
+++ language/tr.php
@@ -1,7 +1,7 @@
 <?php
 $lang = [
   'title' => 'Başlık',
-
+  'Sign in to start your session' => 'Oturumu Başlatmak için Giriş Yapınız'
 
 
 
