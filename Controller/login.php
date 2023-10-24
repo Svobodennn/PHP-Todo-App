49e06746477d6e3165189313da0dd6dd48009e3e
Svobodennn <melih.sarac@hotmail.com>
2023-10-24 14:43:03 +0300

    Login operations, error message handling, creating sessions

--- Controller/login.php
+++ Controller/login.php
@@ -1,12 +1,22 @@
 <?php
-if (route(0) === 'login'){
+if (get_session('login')){
+    redirect('home');
+}
 
-    if (isset($_POST['submit'])){
+if (route(0) === 'login') {
+
+    if (isset($_POST['submit'])) {
+        $_SESSION['post'] = $_POST;
+
         $mail = post('mail');
         $password = post('password');
+        $result = model('auth/login', ['mail' => $mail, 'password' => $password], 'login');
+
+        if ($result['success']) {
+            redirect('home');
+        } else {
+            add_session('message', $result['message']);
+        }
     }
-
     view('auth/login');
-
-
 }
