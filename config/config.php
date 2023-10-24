d4851a9ea3b5a747f2f6b2c039902af58ff3b999
Svobodennn <melih.sarac@hotmail.com>
2023-10-24 14:41:26 +0300

    Database connection

--- config/config.php
+++ config/config.php
@@ -2,3 +2,9 @@
 const BASEDIR = 'C:\xampp\htdocs\PHP-Todo-App';
 const URL = "http://localhost/PHP-Todo-App/";
 const DEV_MODE = true;
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=todoapp', 'root', '');
+} catch (PDOException $e) {
+    $e->getMessage();
+}
