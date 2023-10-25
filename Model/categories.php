5d5230937b73c6c81a18a56feea7a10061f96cfe
Svobodennn <melih.sarac@hotmail.com>
2023-10-25 12:35:52 +0300

    List Categories Model

--- Model/categories.php
+++ Model/categories.php
@@ -15,7 +15,7 @@ if ($process == 'add') {
     $q = $db->prepare('insert into categories set title= :title, user_id= :user_id');
     $q->execute([
         'title' => $data['title'],
-        'user_id' => $_SESSION,
+        'user_id' => get_session('id'),
     ]);
 
     if ($q->rowCount()) {
@@ -23,13 +23,49 @@ if ($process == 'add') {
             'success' => true,
             'message' => 'Category Succesfully Created.',
             'type' => 'info',
-            'data' => get_session('id')
         ];
     } else {
         return [
             'success' => false,
             'message' => "Something Went Wrong",
             'type' => 'danger'
+        ];
+    }
+}
+elseif ($process == 'list') {
+
+
+    $q = $db->prepare('select * from categories where user_id= :user_id');
+    $q->execute([
+        'user_id' => get_session('id'),
+    ]);
+
+    if ($q->rowCount()) {
+        return [
+            'success' => true,
+            'data' => $q->fetchAll(PDO::FETCH_ASSOC)
+        ];
+    } else {
+        return [
+            'success' => false
+        ];
+    }
+}
+elseif ($process == 'remove') {
+
+    $id = $data['category_id'];
+    $q = $db->prepare('delete from categories where id= :id');
+    $q->execute([
+        'id' => $id,
+    ]);
+
+    if ($q->rowCount()) {
+        return [
+            'success' => true,
+        ];
+    } else {
+        return [
+            'success' => false
         ];
     }
 }
